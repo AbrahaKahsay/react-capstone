47c681ce47df92258d49f46dd59be775630d161a
Abraha Kahsay <abrahak81@gmail.com>
2022-08-31 16:33:53 +0300

    Add Navbar component

---
+++ src/components/Navbar.js
@@ -0,0 +1,21 @@
+import { NavLink } from 'react-router-dom';
+import Back from '../images/back.png';
+import Mic from '../images/mic.png';
+import Settings from '../images/settings.png';
+
+function Navbar() {
+  return (
+    <nav className='nav-container'>
+        <div className="back">
+            <img className='icons' src={Back} alt="back" />
+        </div>
+        <NavLink className='detail' to="/city">Details</NavLink>
+        <div className='mic-set'>
+            <img className='icons' src={Mic} alt="mic" />
+            <img className='icons' src={Settings} alt="settings" />
+        </div>
+    </nav>
+  )
+}
+
+export default Navbar
