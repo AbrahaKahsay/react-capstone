615513040c51cefdf631b1e98b29b11560322d36
Abraha Kahsay <abrahak81@gmail.com>
2022-08-31 17:50:43 +0300

    Add Actions to coins

--- src/redux/home/Coin.js
+++ src/redux/home/Coin.js
@@ -1,3 +1,8 @@
 // API for coins
 const baseUrl = 'https://api.coincap.io/v2/assets?limit=100';
 
+// Actions
+const COINS_FETCHED = 'COINS_FETCHED';
+const COINS_FILTERED = 'COINS_FILTERED';
+
+const initialState = [];
