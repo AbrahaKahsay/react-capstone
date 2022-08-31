901757a209add848381a7ea75dd407e797fc19a0
Abraha Kahsay <abrahak81@gmail.com>
2022-08-31 18:26:38 +0300

    Install redux-thunk and add configureStore.js

---
+++ src/redux/configureStore.js
@@ -0,0 +1,17 @@
+import {createStore, combineReducers, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import coinReducer from './home/Coin';
+import detailReducer from './home/Detail';
+
+  
+const reducer = combineReducers({
+    coinReducer,
+    detailReducer,
+  });
+  
+const store = createStore(
+    reducer,
+    applyMiddleware(thunk),
+    );
+  
+export default store;
