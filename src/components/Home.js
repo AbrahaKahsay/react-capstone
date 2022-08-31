e40c0e74a08cd9ffc34d16932ba5649ec48d60b4
Abraha Kahsay <abrahak81@gmail.com>
2022-08-31 20:16:35 +0300

    Update Home with Cards

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { coinList, coinFilter } from '../redux/home/Coin';
+import Card from './Card';
 
 const Main = () => {
   const dispatch = useDispatch();
@@ -31,6 +32,17 @@ const Main = () => {
           <input type="submit" value="Search" />
         </form>
       </div>
+      <ul className="coinList">
+        {cList.map((coin) => (
+          <Card
+            key={coin.id}
+            id={coin.id}
+            symbol={coin.symbol}
+            price={coin.priceUsd.substring(0, 8)}
+            change={coin.changePercent24Hr}
+          />
+        ))}
+      </ul>
     </section>
   );
 };
