c970a5ca919e7d6e873e6b9a8d0e711b4602c37d
Abraha Kahsay <abrahak81@gmail.com>
2022-09-01 14:01:53 +0300

    Update Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,10 +7,10 @@ import './styles/Navbar.css';
 function Navbar() {
   return (
     <nav className="nav-container">
+      <NavLink to="/"><img className="icons" src={HomeL} alt="home" /></NavLink>
       <div className="back">
-        <img className="icons" src={HomeL} alt="home" />
+        <h2>Crypto Market</h2>
       </div>
-      <NavLink className="detail" to="/"><h2>Crypto Market</h2></NavLink>
       <div className="mic-set">
         <img className="icons" src={Mic} alt="mic" />
         <img className="icons" src={Settings} alt="settings" />
