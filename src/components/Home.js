d751aacdba3c6079c90d40e3332e22baff0b9771
Abraha Kahsay <abrahak81@gmail.com>
2022-09-01 09:50:06 +0300

    Add div to options and forms

--- src/components/Home.js
+++ src/components/Home.js
@@ -24,14 +24,14 @@ const Home = () => {
   return (
     <section>
       <div className="info-list">
-        <div>
+        <div className="options">
           <select className="filter" onChange={changeHandler}>
             <option value="price"> Rank </option>
             <option value="name"> Name </option>
             <option value="vol"> Price </option>
           </select>
         </div>
-        <div>
+        <div className="forms">
           <form onSubmit={searchHandler}>
             <input type="text" name="coinSearch" placeholder="Crypto coin name" required />
             <input type="submit" value="Search" />
