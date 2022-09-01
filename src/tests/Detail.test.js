bf3401707047ce25ad14e5b9c455a9d463b2d3f8
Abraha Kahsay <abrahak81@gmail.com>
2022-09-01 16:16:55 +0300

    Add test to Detail.js

---
+++ src/tests/Detail.test.js
@@ -0,0 +1,20 @@
+import { Provider } from 'react-redux';
+import renderer from 'react-test-renderer';
+import { BrowserRouter } from 'react-router-dom';
+import store from '../redux/configureStore'
+import Detail from '../components/Detail'
+import { coinDetail } from '../redux/home/Detail'
+
+describe('Jest Snapshot testing suite', () => {
+  test('component match with the snapshot', async () => {
+    await store.dispatch(coinDetail('bitcoin'));
+    const tree = renderer.create(
+      <BrowserRouter>
+        <Provider store={store}>
+          <Detail />
+        </Provider>
+      </BrowserRouter>,
+    ).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
